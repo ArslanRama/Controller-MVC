150db8f2e1a1ed18ce9dc67c3ff5ed8526867a3c
ArslanRama <arslanrama84@gmail.com>
2021-04-27 14:16:19 +0200

    saved

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,12 +1,59 @@
-//! Routing
 const express = require('express');
 const router = express.Router();
 
-const indexController = require('../controllers/indexController');
+router.get('/', (req, res)=>{
+    res.render('home', {
+        pageTitle: 'Home Page',
+        websiteName: 'arif.com',
+        students_num: 17,
+        company_data: { // nested object
+            name: 'DCI',
+            type: 'Education',
+            address: {
+                street: 'Berlineer Alee 100',
+                city: 'Düsseldorf'
+            }
+        },
+        foods: ['routerle', 'orange', 'Grapes', 'jackfruit', 'melon'], // array of strings
+        users: [ // array of objects
+            {
+                id: 1,
+                name: 'Hiba'
+            },
+            {
+                id: 2,
+                name: 'Jose'
+            },
+            {
+                id: 3,
+                name: 'Syed'
+            }
+        ]
+    }); 
+})
 
-router.get('/', indexController.index);
-router.get('/contact', indexController.contact);
+router.get('/contact', (req, res)=>{
+    res.render('contact', {pageTitle: 'Contact Page'});
+})
 
-router.get('/gallery', indexController.gallery);
+// receive some data from form/user
+router.post('/formdata', (req, res)=> {
+    // data we received
+    // req.body
+    console.log('Data from user:', req.body)
+    // put data in a variable
+    //const {email, username, message} = req.body;
+    // send/render/redirect this data to profile page
+    //res.render('profile', {user: req.body})
+    res.render('contact', {
+        pageTitle: 'Contact Page',
+        user: req.body
+    })
+})
 
-module.exports = router
+router.get('/gallery', (req, res)=>{
+    res.render('gallery');
+    //res.sendFile(__dirname+'/html/gallery.html')
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //! Declaration
 const express = require('express');
 const app = express();
+const port=5000;
 const userRouter = require('./routes/userRouter');
 const productRouter = require('./routes/productRouter');
 const indexRouter = require('./routes/indexRouter');
@@ -11,8 +12,20 @@ app.use(express.static(__dirname+'/public'))
 // app.set method saying we have a view engine called hbs
 app.set('view engine', 'hbs');
 
+app.use(express.urlencoded({
+    extended: false
+}))
 
 //! Routing
+app.get('/login', (req, res)=>{
+    res.render('login');
+ })
+ app.post('/login', (req, res)=> {
+     console.log(req.body)
+     res.json(req.body); // display js object in browser as json data
+ })
+
+
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 app.use('/product', productRouter);
@@ -21,8 +34,8 @@ app.use('/product', productRouter);
 
 
 //! listen app with port
-app.listen(5000, ()=>{
-    console.log('Server is running at localhost:5000')
+app.listen(port, ()=>{
+    console.log(`Server is running at http://localhost:${port}`)
 })
 
 
